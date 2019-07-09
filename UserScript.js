b807e14900014a3acf5e14496b866dbb3bb344ef
Kakei Akihiko <kakei.akihiko@gmail.com>
2019-07-10 06:35:39 +0900

    現在のページ番号を表示

--- UserScript.js
+++ UserScript.js
@@ -231,12 +231,17 @@ site.setup();
 
 const PagingBlock = {
   template: `
-    <div class="d-flex v-interval">
-      <button class="btn btn-link p-0" @click="$emit('click', page)">再読み込み</button>
-      <button class="btn btn-link p-0" @click="$emit('click', 1)" v-if="page > 1">最新を取得</button>
-      <button class="btn btn-link p-0" @click="$emit('click', page - 1)" v-if="page > 1">← 前の25件</button>
-      <button class="btn btn-link p-0" @click="$emit('click', page + 1)">→ 次の25件</button>
-      <button class="btn btn-link p-0" @click="$emit('click', page + 5)">古い方へ+5p</button>
+    <div class="d-flex justify-content-between v-interval">
+      <div class="v-interval">
+        <button class="btn btn-link p-0" @click="$emit('click', page)">再読み込み</button>
+        <button class="btn btn-link p-0" @click="$emit('click', 1)" v-if="page > 1">最新を取得</button>
+        <button class="btn btn-link p-0" @click="$emit('click', page - 1)" v-if="page > 1">← 前の25件</button>
+        <button class="btn btn-link p-0" @click="$emit('click', page + 1)">→ 次の25件</button>
+        <button class="btn btn-link p-0" @click="$emit('click', page + 5)">古い方へ+5p</button>
+      </div>
+      <div class="v-interval text-right" style="color: rgb(100,100,100); font-size: small">
+        p.{{page}}
+      </div>
     </div>
   `,
   name: 'paging-block',
