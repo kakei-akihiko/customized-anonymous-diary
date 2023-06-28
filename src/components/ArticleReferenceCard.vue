f09ef2c4e871a1111611d8bbe10c6477781f23b1
Kakei Akihiko <kakei.akihiko@gmail.com>
2023-06-29 06:58:38 +0900

    言及先タイトルの大きさ調整

--- src/components/ArticleReferenceCard.vue
+++ src/components/ArticleReferenceCard.vue
@@ -12,11 +12,8 @@ export default {
 </script>
 
 <template>
-  <div
-    class="card pt-2 pl-2 pr-2 mb-2"
-    style="background-color: honeydew"
-  >
-    <div class="card-title">
+  <div class="card pt-2 pl-2 pr-2 mb-2 refer-entire">
+    <div class="card-title refer-title">
       <a :href="url">■</a>
       <strong>{{ title }}</strong>
     </div>
@@ -25,3 +22,12 @@ export default {
     </div>
   </div>
 </template>
+
+<style scoped>
+.refer-entire {
+  background-color: honeydew
+}
+.refer-title {
+  font-size: 1.7rem
+}
+</style>
