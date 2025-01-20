b8c52cd3c063200419bd5f9f280254fc5052cfbf
Kakei Akihiko <kakei.akihiko@gmail.com>
2025-01-20 22:59:18 +0900

    NGワード表示領域

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 import ArticleCard from './components/ArticleCard.vue'
 import PagingBlock from './components/PagingBlock.vue'
 import { updateReference } from './usecases/reference.js'
 import { entriesRef, fetchEntries } from './usecases/data'
 
 const scroll = ref(null)
+
+const rightSidePanelCollapsedRef = ref(false)
 
 onMounted(async () => {
   await fetchEntries()
@@ -20,13 +22,29 @@ const pagingClick = page => {
 const referButtonClick = entry => {
   updateReference(entry)
 }
+
+const rightSidePanel = computed(() => {
+  return {
+    className: {
+      'panel-right-side': true,
+      'collapsed': rightSidePanelCollapsedRef.value
+    }
+  }
+})
+
+const rightSidePanelToggleButtonClick = () => {
+  rightSidePanelCollapsedRef.value = !rightSidePanelCollapsedRef.value
+}
 </script>
 
 <template>
   <div
     ref="scroll"
-    class="h-100 scroll"
+    class="h-100 entire"
   >
+    <div>
+
+    </div>
     <div class="container container-main">
       <PagingBlock @change="pagingClick($event)" />
       <ArticleCard
@@ -37,5 +55,55 @@ const referButtonClick = entry => {
       />
       <PagingBlock @change="pagingClick($event)" />
     </div>
+    <div :class="rightSidePanel.className">
+      <button
+        class="btn btn-link btn-open"
+        @click="rightSidePanelToggleButtonClick"
+      >
+        ≡
+      </button>
+      <div class="panel-collapsed">
+        <button
+          class="btn btn-link btn-collapse"
+          @click="rightSidePanelToggleButtonClick"
+        >
+          ≡
+        </button>
+        NGワード
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.entire {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+}
+
+/* ≡ボタン 右サイドバー非表示状態ではウインドウに対して固定表示される */
+.panel-right-side .btn-open {
+  position: absolute;
+  font-size: 1.8rem;
+}
+
+/* ≡ボタン 右サイドバー表示状態では表示されない */
+.panel-right-side.collapsed .btn-open {
+  display: none;
+}
+
+/* 右サイドバーの内容（非表示） */
+.panel-right-side .panel-collapsed {
+  display: none;
+}
+
+/* 右サイドバーの内容（表示） */
+.panel-right-side.collapsed .panel-collapsed {
+  display: block;
+  font-size: 1.8rem;
+}
+.btn-collapse {
+  display: block;
+  font-size: 1.8rem;
+}
+</style>
