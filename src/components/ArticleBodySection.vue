4010ad6a44ca9a1ceecb766b2b40eee6491ec085
Kakei Akihiko <kakei.akihiko@gmail.com>
2025-02-26 04:52:53 +0900

    ArticleBodySectionItem.vue

--- src/components/ArticleBodySection.vue
+++ src/components/ArticleBodySection.vue
@@ -1,6 +1,9 @@
 <script>
+import ArticleBodySectionItem from './ArticleBodySectionItem.vue';
+
 export default {
   name: 'ArticleSection',
+  components: { ArticleBodySectionItem },
   props: {
     items: { type: Array, required: true }
   }
@@ -13,32 +16,7 @@ export default {
       v-for="item in items"
       :key="item.nodeIndex"
     >
-      <p v-if="item.nodeName == 'P'">
-        {{ item.text }}
-      </p>
-      <ul v-if="item.nodeName == 'UL'">
-        <li
-          v-for="text in item.texts"
-          :key="text.index"
-        >
-          {{ text.text }}
-        </li>
-      </ul>
-      <ol v-if="item.nodeName == 'OL'">
-        <li
-          v-for="text in item.texts"
-          :key="text.index"
-        >
-          {{ text.text }}
-        </li>
-      </ol>
-      <pre
-        v-if="item.nodeName == 'PRE'"
-        class="rounded p-1"
-        style="background-color: #e7ebff"
-      >
-        {{ item.text }}
-      </pre>
+      <ArticleBodySectionItem :item="item" />
       <blockquote
         v-if="item.nodeName == 'BLOCKQUOTE'"
         class="rounded p-1"
@@ -49,75 +27,10 @@ export default {
             v-for="child in item.children"
             :key="child.nodeIndex"
           >
-            <p v-if="child.nodeName == 'P'">
-              {{ child.text }}
-            </p>
-            <ul v-if="child.nodeName == 'UL'">
-              <li
-                v-for="text in child.texts"
-                :key="text.index"
-              >
-                {{ text.text }}
-              </li>
-            </ul>
-            <ol v-if="child.nodeName == 'OL'">
-              <li
-                v-for="text in child.texts"
-                :key="text.index"
-              >
-                {{ text.text }}
-              </li>
-            </ol>
-            <pre
-              v-if="child.nodeName == 'PRE'"
-              class="rounded p-1"
-              style="background-color: #e7ebff"
-            >
-              {{ child.text }}
-            </pre>
-            <h4
-              v-if="child.nodeName == 'H4'"
-              class="h5 ml-0"
-            >
-              {{ child.text }}
-            </h4>
-            <h5
-              v-if="child.nodeName == 'H5'"
-              class="h6 ml-0"
-            >
-              {{ child.text }}
-            </h5>
-            <h6
-              v-if="child.nodeName == 'H6'"
-              class="h6 ml-0"
-            >
-              {{ child.text }}
-            </h6>
+            <ArticleBodySectionItem :item="child" />
           </div>
         </div>
       </blockquote>
-      <h4
-        v-if="item.nodeName == 'H4'"
-        class="h5 ml-0"
-      >
-        {{ item.text }}
-      </h4>
-      <h5
-        v-if="item.nodeName == 'H5'"
-        class="h6 ml-0"
-      >
-        {{ item.text }}
-      </h5>
-      <h6
-        v-if="item.nodeName == 'H6'"
-        class="h6 ml-0"
-      >
-        {{ item.text }}
-      </h6>
-      <details v-if="item.unknownType">
-        <summary>{{ item.text }}</summary>
-        {{ item.html }}
-      </details>
     </div>
   </div>
 </template>
---
+++ src/components/ArticleBodySectionItem.vue
@@ -0,0 +1,56 @@
+<script setup>
+const props = defineProps({
+  item: Object
+})
+</script>
+
+<template>
+  <p v-if="props.item.nodeName == 'P'">
+    {{ props.item.text }}
+  </p>
+  <ul v-if="props.item.nodeName == 'UL'">
+    <li
+      v-for="text in props.item.texts"
+      :key="text.index"
+    >
+      {{ text.text }}
+    </li>
+  </ul>
+  <ol v-if="props.item.nodeName == 'OL'">
+    <li
+      v-for="text in props.item.texts"
+      :key="text.index"
+    >
+      {{ text.text }}
+    </li>
+  </ol>
+  <pre
+    v-if="props.item.nodeName == 'PRE'"
+    class="rounded p-1"
+    style="background-color: #e7ebff"
+  >
+    {{ props.item.text }}
+  </pre>
+  <h4
+    v-if="props.item.nodeName == 'H4'"
+    class="h5 ml-0"
+  >
+    {{ props.item.text }}
+  </h4>
+  <h5
+    v-if="props.item.nodeName == 'H5'"
+    class="h6 ml-0"
+  >
+    {{ props.item.text }}
+  </h5>
+  <h6
+    v-if="props.item.nodeName == 'H6'"
+    class="h6 ml-0"
+  >
+    {{ props.item.text }}
+  </h6>
+  <details v-if="props.item.unknownType">
+    <summary>{{ props.item.text }}</summary>
+    {{ props.item.html }}
+  </details>
+</template>
