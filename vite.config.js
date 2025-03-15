6b5f570170f6d6e9098388f9028eca0858af8917
Kakei Akihiko <kakei.akihiko@gmail.com>
2025-03-15 09:53:36 +0900

    3.0.1 viteを更新

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       content: `// ==UserScript==
 // @name         カスタマイズ版匿名日記
 // @namespace    http://tampermonkey.net/
-// @version      3.0.0
+// @version      3.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://anond.hatelabo.jp/customized
