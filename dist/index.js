f2ed9a87be5a3ba6bf4fccfb4632dbd9733e147a
Kakei Akihiko <kakei.akihiko@gmail.com>
2025-04-03 00:28:00 +0900

    3.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         カスタマイズ版匿名日記
 // @namespace    http://tampermonkey.net/
-// @version      3.0.3
+// @version      3.0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://anond.hatelabo.jp/customized
