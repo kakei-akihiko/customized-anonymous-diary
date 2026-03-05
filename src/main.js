ef73fc0a0e779e7af694a6567a667ad9dfe78ef9
Kakei Akihiko <kakei.akihiko@gmail.com>
2026-03-06 06:05:55 +0900

    main.jsリファクタリング

--- src/main.js
+++ src/main.js
@@ -5,15 +5,20 @@ import { addCss } from './presenter/css'
 
 addCss()
 
-const bodyChildren = Array.from(document.body.childNodes)
-
 document.body.insertAdjacentHTML(
   'beforeend',
   '<div id="original"></div><div id="app"></div>'
 )
 
-bodyChildren.forEach(node => {
-  document.getElementById('original').appendChild(node)
-})
+const originalParentNode = document.getElementById('original')
+
+// #original, #app以外は#original配下に移動
+Array.from(document.body.childNodes)
+  .filter(node =>
+    node.nodeName === '#text' || (
+      node.getAttribute('id') !== 'original' &&
+      node.getAttribute('id') !== 'app'
+    )
+  ).forEach(node => originalParentNode.appendChild(node))
 
 createApp(App).mount('#app')
