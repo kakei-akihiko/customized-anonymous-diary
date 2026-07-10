bb744e06cc604e7a69cbc0da088f0954abc91b07
Kakei Akihiko <kakei.akihiko@gmail.com>
2026-07-11 00:59:03 +0900

    parseFooter: 引数を.sectionのノードに変更、null対応、コメント追加

--- src/infrastructure/html/parseFooter.js
+++ src/infrastructure/html/parseFooter.js
@@ -1,14 +1,16 @@
 
-// 各記事のフッターからデータを取得
-export const parseFooter = footerNode => {
+// 各記事のノード（.section）からフッターのデータを取得
+export const parseFooter = sectionNode => {
+  const footerNode = sectionNode.querySelector(':scope .sectionfooter')
+
   // 記事への反応(1)などの数字を取得
-  const anchors = footerNode.getElementsByTagName('a')
+  const anchors = footerNode?.getElementsByTagName('a') ?? []
   const refersText = anchors.length >= 2 ? anchors[1].textContent : ''
   const refersMatch = refersText.match(/\((\d+)\)/)
-  const refersCount = refersMatch == null ? null : refersMatch[1]
+  const refersCount = refersMatch == null ? null : parseInt(refersMatch[1])
 
   // 時刻
-  const children = footerNode.childNodes
+  const children = footerNode?.childNodes ?? []
   const timeNode = children.length > 0 ? children[children.length - 1] : null
   const timeText = timeNode == null ? '' : timeNode.textContent
   const timeMatch = timeText.match(/\d\d:\d\d/)
