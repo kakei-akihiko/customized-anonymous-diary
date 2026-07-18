3c84f2d4caa48698bc310f87c7805d38d3bcb4a7
Kakei Akihiko <kakei.akihiko@gmail.com>
2026-07-19 00:23:19 +0900

    getParagraphs

--- src/infrastructure/anond/DocumentParser.js
+++ src/infrastructure/anond/DocumentParser.js
@@ -1,3 +1,4 @@
+import { getParagraphs } from '../html/sectionNode/Paragraphs.js'
 import { parseHeader } from '../html/parseHeader.js'
 import { parseFooter } from '../html/parseFooter.js'
 
@@ -12,7 +13,7 @@ class DocumentParser {
 
     const { refersCount, time } = parseFooter(node)
 
-    const paragraphs = this.getArticleBody(node)
+    const paragraphs = getParagraphs(node)
 
     const html = Array.from(node.childNodes)
       .filter(n => n.nodeType === Node.ELEMENT_NODE)
@@ -41,80 +42,6 @@ class DocumentParser {
       }
     }
     return false
-  }
-
-  getArticleBody (node) {
-    return Array.from(node.childNodes)
-      .map((child, index) => this.parseArticleBodyLine(index, child))
-      .filter(item => item != null)
-      .reduce((results, item) => {
-        // 連続する解析エラーを1つにまとめる
-        const lastItem = results.length > 0 ? results[results.length - 1] : null
-        const unknownTypeProcessing = lastItem?.unknownType ?? false
-        if (item.unknownType && unknownTypeProcessing) {
-          lastItem.html += item.html
-        } else {
-          results.push(item)
-        }
-        return results
-      }, [])
-  }
-
-  parseArticleBodyLine (nodeIndex, articleChildNode) {
-    const nodeName = articleChildNode.nodeName
-    switch (nodeName) {
-      case 'BR': return null
-      case 'P':
-        const classes = Array.from(articleChildNode.classList)
-        if (classes.includes('sectionfooter') || classes.includes('share-button')) {
-          return null
-        }
-        return {
-          className: articleChildNode.className,
-          nodeIndex,
-          text: articleChildNode.textContent,
-          nodeName
-        }
-      case 'UL':
-      case 'OL': {
-        const items = articleChildNode.querySelectorAll('li')
-        const texts = Array.from(items).map((node, index) => {
-          const text = node.textContent
-          return { index, text }
-        })
-        return { nodeIndex, texts, nodeName }
-      }
-      case 'BLOCKQUOTE':
-        return {
-          nodeIndex,
-          children: this.getArticleBody(articleChildNode),
-          nodeName
-        }
-      case 'HR':
-        return {
-          nodeIndex,
-          nodeName
-        }
-      case 'PRE':
-      case 'H4':
-      case 'H5':
-      case 'H6':
-        return { nodeIndex, text: articleChildNode.textContent, nodeName }
-      case 'H3':
-      case '#text':
-        return null
-      default:
-        if (nodeName === 'DIV' && articleChildNode.id === 'rectangle-middle') {
-          return null
-        }
-        return {
-          nodeIndex,
-          text: '解析エラー',
-          nodeName,
-          unknownType: true,
-          html: articleChildNode.outerHTML
-        }
-    }
   }
 }
 
